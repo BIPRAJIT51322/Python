6dbbdba7da07c1046850d0cdc193af41deb88913
BIPRAJIT51322 <56039295+BIPRAJIT51322@users.noreply.github.com>
2020-06-03 11:30:05 -0700

    Create main.py

---
+++ Maximum and Minimum/main.py
@@ -0,0 +1,12 @@
+print("This program will show you the maximum and minimum number amoung four numbers")
+print("Enter the first number")
+a = int(input())
+print("Enter the second number")
+b = int(input())
+print("Enter the third number")
+c = int(input())
+print("Enter the fourth number")
+d = int(input())
+list = [a , b , c, d]
+print(max(list), "is the maximum number")
+print(min(list), "is the minimum number")
